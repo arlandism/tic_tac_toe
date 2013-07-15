323764e417e9631aeb5aacb763c079fc1c5d57d0
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-07-15 14:25:33 -0500

    Added BoardGenerator

---
+++ board_generator.py
@@ -0,0 +1,31 @@
+
+class BoardStringGenerator(object):
+
+    def __init__(self,index,state):
+        self.index = index
+        self.cells = [cell for cell in range(1,self.index * self.index+1)]
+        self.state = state
+      
+    def generate_layout(self):
+        keys_present = self.state.keys()
+        keys_not_present = set(self.cells) - set(keys_present)
+        layout = dict()
+        for key in keys_present:
+            layout[str(key)] = self.state[key]
+        for key in keys_not_present:
+             layout[str(key)] = ""
+        return layout
+
+    def generate_template(self):
+        board_template = []
+        spaces_per_token = 3
+        dashes = "-" * (spaces_per_token * self.index)
+        left_side = "%("
+        right_side = ")3s"
+        for number in self.cells:
+          if number % self.index == 0:
+              board_template.append(left_side + str(number) + right_side + "\n" + dashes + "\n")
+          else:
+              board_template.append(left_side + str(number) + right_side)
+        board_template = "".join(board_template)
+        return board_template.rstrip("\n" + dashes) 
