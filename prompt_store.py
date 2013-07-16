0484aaf06c857f95c47ac47712cc8a0ef93e7ba9
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-07-15 21:38:55 -0500

    Fixed string duplication in prompt store

--- prompt_store.py
+++ prompt_store.py
@@ -2,9 +2,9 @@ from collections import OrderedDict
 
 class PromptStore(object):
 
+    player_choices = ("Human","Humanoid","ImpossibleAI","EasyAi")
     game_prompts = OrderedDict()
-    player_string = ("\nHuman\n" + "Humanoid\n" + "ImpossibleAI\n" + "EasyAi\n")
     game_prompts["What's the first player's token (x,o)? "] = ("x","o")
-    game_prompts["The first player is a..." + player_string] = ("Human","Humanoid","ImpossibleAI","EasyAi")
-    game_prompts["And the second... same options "] = ("Human","Humanoid","ImpossibleAI","EasyAi")
+    game_prompts["The first player is a...\n" + "\n".join(player_choices)] = player_choices 
+    game_prompts["And the second... same options "] = player_choices 
     game_prompts["Board size please... "] = (3,4)
