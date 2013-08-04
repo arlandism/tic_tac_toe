608e4e8a816ee641e3eb67a3dca0074094c7db45
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-03 23:44:23 -0500

    Moved server starting script to network_io directory

---
+++ network_io/start_server.py
@@ -0,0 +1,13 @@
+from server_socket import ServerSocket
+from json_transmitter import JsonTransmitter
+from responder import Responder
+
+server = ServerSocket("localhost",5000)
+server.initialize_and_listen_for_connections()
+
+while True:
+  connection_socket = server.accept_connection_and_return_socket()
+  transmitter = JsonTransmitter(connection_socket)
+  responder = Responder(transmitter)
+  responder.respond()
+  connection_socket.close()
