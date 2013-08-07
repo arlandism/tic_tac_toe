669b68cae9c315dc1788665c3abd72b1231836c5
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-06 22:26:44 -0500

    Deletes hand-rolled mocks and unnecessary imports. Moves integration tests into separate file.

--- network_io/test/test_responder.py
+++ network_io/test/test_responder.py
@@ -1,67 +1,40 @@
 import unittest
 import mock
 from responder import Responder, MoveGenerator
-from web_io_test_utils import MockTransmitter
-
-class FakeMoveGenerator(object):
-
-    def __init__(self, to_return,board=None):
-        self.to_return = to_return
-        self.argument_history = []
-
-    def next_move(self, board_state):
-        self.argument_history.append(board_state)
-        return self.to_return
-
-    def winner(self):
-        pass
 
 class ResponderTests(unittest.TestCase):
 
-     def test_responds_got_something(self):
-       transmitter = MockTransmitter([{"something":""}])
-       responder = Responder(transmitter,mock.Mock())
-       responder.respond()
-       self.assertEqual(1,transmitter.times_receive_called)
-
-     def test_respond_sent_something(self):
-       transmitter = MockTransmitter(["stuff"])
-       generator = FakeMoveGenerator([""])
-       responder = Responder(transmitter,generator)
-       responder.respond()
-       self.assertEqual(2,transmitter.times_sent_called)
-
-class ResponderIntegrationTests(unittest.TestCase):
-
-    easy_win_possible = {1:"o", 2:"o", 4:"x", 7:"x"}
-    bottom_row_win_possible = {7:"o", 8:"o"}
-
-    #def test_returns_board_with_obvious_move(self):
-    #    transmitter = mock.Mock()
-    #    transmitter.receive = mock.MagicMock(return_value=self.easy_win_possible)
-    #    minimax = mock.Mock()
-    #    minimax.next_move = mock.MagicMock(return_value=3)
-    #    generator = MoveGenerator(minimax=minimax)
-    #    responder = Responder(transmitter,generator)
-    #    responder.respond()
-    #    transmitter.send.assert_called_with(3, "o")
-
-    def test_response_with_best_move(self):
-        transmitter = MockTransmitter([self.easy_win_possible])
-        generator = FakeMoveGenerator(5)
-        responder = Responder(transmitter,generator) 
+    def test_responds_got_something(self):
+        transmitter = mock.Mock()
+        transmitter.receive = mock.MagicMock()
+        responder = Responder(transmitter,mock.Mock())
         responder.respond()
-        self.assertEqual(generator.to_return, 5)
+        self.assertEqual(1,transmitter.receive.call_count)
 
-    def test_calls_next_move_with_board_state(self):
-        transmitter = MockTransmitter([self.easy_win_possible])
-        generator = FakeMoveGenerator(5)
-        responder = Responder(transmitter,generator) 
+    def test_respond_sent_something(self):
+        transmitter = mock.Mock()
+        transmitter.send = mock.MagicMock()
+        generator = mock.Mock()
+        responder = Responder(transmitter,generator)
         responder.respond()
-        self.assertGeneratorNextMoveCalledWith(generator,self.easy_win_possible)
+        self.assertEqual(2,transmitter.send.call_count)
 
-    def assertGeneratorNextMoveCalledWith(self,generator,called_with):
-        self.assertEqual(generator.argument_history[0], called_with)
+    def test_respond_sends_next_move_from_generator(self):
+        transmitter = mock.Mock()
+        generator = mock.Mock()
+        generator.winner = mock.MagicMock(return_value=None)
+        generator.next_move = mock.MagicMock(return_value=3)
+        responder = Responder(transmitter,generator)
+        responder.respond() 
+        transmitter.send.assert_any_call(3)
+         
+    def test_respond_sends_generator_winner(self):
+        transmitter = mock.Mock() 
+        generator = mock.Mock()
+        generator.winner = mock.MagicMock(return_value="x")
+        responder = Responder(transmitter, generator)
+        responder.respond()
+        transmitter.send.assert_called_with("x") 
 
 class MoveGeneratorTests(unittest.TestCase):
 
