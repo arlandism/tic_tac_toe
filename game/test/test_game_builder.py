0394ea4c9418cd5cf1d233660c42aaebe7320b65
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-13 15:49:48 -0500

    Decouples tests from io package by using mock library

--- game/test/test_game_builder.py
+++ game/test/test_game_builder.py
@@ -1,43 +1,40 @@
 import unittest
+import mock
 
 from game.game_builder import GameBuilder
 from game.game import Game
-from io.prompt_parser import PromptParser
-from io.prompt_store import PromptStore
-
-from game.test.test_utils import MockFactory
-from test.test_utils import MockParser, FakePrinter, SimpleMockPrompter
 
 class GameBuilderTests(unittest.TestCase):
-
-    def setUp(self):
-        self.data = {"Player One": "Fake",
-                     "Player Two": "Fake",
-                     "board":20}
-        self.fake_data = {PromptStore().player_string(): "Human",
-                          PromptStore().first_token():"x",
-                          PromptStore().second_player():"Human",
-                          PromptStore().board():4}
-        self.parser = MockParser(self.data)
+        
+    def build_fake_parser(self):
+        parser = mock.Mock()
+        parser.first_player = mock.MagicMock(return_value="Human")
+        parser.first_token = mock.MagicMock(return_value="x")
+        parser.second_player = mock.MagicMock(return_value="Human")
+        parser.second_token = mock.MagicMock(return_value="o")
+        parser.board_size = mock.MagicMock(return_value=4)
+        return parser
 
     def test_used_parser_passed_in(self):
-        game = GameBuilder.game(self.parser)
+        parser = mock.Mock()
+        parser.board_size = mock.MagicMock(return_value=20)
+        game = GameBuilder.game(parser)
         self.assertEqual(20,game.board.board_index)
 
     def test_factory_called(self):
-        self.data["Player One"] = "FakePlayerOne"
-        factory = MockFactory  
-        game = GameBuilder.game(self.parser,factory=factory)
-        self.assertEqual("FakePlayerOne",game.player_one)
+        factory = mock.Mock()
+        parser = self.build_fake_parser()
+        game = GameBuilder.game(parser,factory=factory)
+        self.assertEqual(2, factory.player.call_count)
 
-    def test_display_object_passed_in(self):
-        prompter = SimpleMockPrompter([]) 
-        parser = PromptParser(self.fake_data)
+    def test_display_object_passed_in_to_game(self):
+        prompter = mock.Mock() 
+        parser = self.build_fake_parser()
         game = GameBuilder.game(parser,prompter)
         self.assertEqual(prompter,game.prompter)
 
     def test_display_object_reaches_factory_and_players(self):
-        printer = FakePrinter()
-        parser = PromptParser(self.fake_data)
+        printer = mock.Mock()
+        parser = self.build_fake_parser()
         game = GameBuilder.game(parser,prompter=printer)
         self.assertEqual(printer,game.player_one.prompter)
