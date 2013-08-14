bc71fc274be873855d3e3f66b8e144e3cc3a75f2
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-13 19:49:30 -0500

    Adds config parser w/ tests

--- network_io/config_parser.py
+++ network_io/config_parser.py
@@ -2,4 +2,23 @@
 class ConfigParser(object):
 
     def __init__(self,file_to_parse):
-        pass
+        self.to_parse = file_to_parse
+         
+    def token(self):
+        return self.__find_it("token")
+
+    def player_one(self):
+        return self.__find_it("player_one")
+
+    def player_two(self):
+        return self.__find_it("player_two")
+
+    def difficulty(self):
+        return self.__find_it("difficulty")
+        
+    def __find_it(self,to_find):
+        PREFIX_LEN = len(to_find) + len(": ")
+        start = self.to_parse.find(to_find + ": ") + PREFIX_LEN
+        END_OF_VAL = self.to_parse[start: ].find("\n")
+        found = self.to_parse[start:start + END_OF_VAL] 
+        return found
--- network_io/test/test_config_parser.py
+++ network_io/test/test_config_parser.py
@@ -3,5 +3,26 @@ from config_parser import ConfigParser
 
 class ConfigParserTests(unittest.TestCase):
 
-    def test_instantiation(self):
-        parser = ConfigParser("config.txt")
+    fake_config_file = ("token: test_token\n" +
+                        "player_one: test_player_one\n" +
+                        "player_two: test_player_two\n" +
+                        "difficulty: juggernaut\n")
+
+    def setUp(self):
+        self.parser= ConfigParser(self.fake_config_file)
+
+    def test_reads_from_input_for_token(self):
+        token = self.parser.token()
+        self.assertEqual("test_token",token)
+        
+    def test_reads_from_input_for_player_one(self):
+        player = self.parser.player_one()
+        self.assertEqual("test_player_one",player)
+        
+    def test_reads_from_input_for_player_two(self):
+        player = self.parser.player_two()
+        self.assertEqual("test_player_two",player)
+
+    def test_reads_from_input_for_difficulty(self):
+        difficulty = self.parser.difficulty()
+        self.assertEqual("juggernaut",difficulty)
