76539659a6a55fe342970a1cdac2ffe06fe3dcf1
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-18 21:12:14 -0500

    Moves Responder to top of file

--- network_io/responder.py
+++ network_io/responder.py
@@ -3,22 +3,16 @@ sys.path.append("/Users/arlandislawrence/development/python/tic_tac_toe/")
 from game.minimax import Minimax
 from game.base_board import BaseBoard
 
-class MoveGenerator(object):
+class Responder(object):
+  
+    def __init__(self,transmitter,handler):
+        self.transmitter = transmitter
+        self.handler = handler
 
-    def __init__(self,board=None):
-        if board is None:  board = BaseBoard(3)
-        self.board = board
-
-    def next_move(self,board_state,ai_depth=None):
-        if ai_depth is None:
-            ai_depth = 20
-        self.board.board_state = board_state
-        comp_move = Minimax("o",ai_depth).next_move(self.board)
-        self.board.board_state[comp_move] = "o"
-        return comp_move
-
-    def winner(self):
-        return self.board.winner()
+    def respond(self):
+        transmitter_data = self.transmitter.receive()
+        response = self.handler.response(transmitter_data)
+        self.transmitter.send(response)
 
 class ResponseHandler(object):
 
@@ -39,14 +33,20 @@ class ResponseHandler(object):
         except KeyError:
            difficulty = None
         return difficulty
- 
-class Responder(object):
-  
-    def __init__(self,transmitter,handler):
-        self.transmitter = transmitter
-        self.handler = handler
 
-    def respond(self):
-        transmitter_data = self.transmitter.receive()
-        response = self.handler.response(transmitter_data)
-        self.transmitter.send(response)
+class MoveGenerator(object):
+
+    def __init__(self,board=None):
+        if board is None:  board = BaseBoard(3)
+        self.board = board
+
+    def next_move(self,board_state,ai_depth=None):
+        if ai_depth is None:
+            ai_depth = 20
+        self.board.board_state = board_state
+        comp_move = Minimax("o",ai_depth).next_move(self.board)
+        self.board.board_state[comp_move] = "o"
+        return comp_move
+
+    def winner(self):
+        return self.board.winner()
