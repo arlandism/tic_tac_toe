e013b860c6bf9e6a99117d7c0e921e48ac8c263d
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-04 10:34:41 -0500

    More unit tests for ResponseHandler

--- network_io/test/test_response_handler.py
+++ network_io/test/test_response_handler.py
@@ -6,7 +6,21 @@ from move_generator import MoveGenerator
 
 class ResponseHandlerTests(unittest.TestCase):
 
-   def test_handler_returns_human_vs_human_winner(self):
+   def test_handler_returns_with_move_key(self):
+       data = {"board":""}
+       generator = mock.Mock()
+       generator.next_move = mock.MagicMock(return_value="ai_move")
+       response = ResponseHandler(generator).response(data)
+       self.assertEqual("ai_move",response["move"])
+
+   def test_handler_returns_winner_after_ai_move_key(self):
+       data = {"board":""}
+       generator = mock.Mock()
+       generator.winner = mock.MagicMock(return_value="winner!")
+       response = ResponseHandler(generator).response(data)
+       self.assertEqual("winner!",response["winner"])
+
+   def test_handler_returns_winner_on_board_key(self):
        data = {"board":"stuff"}
        generator = mock.Mock()
        generator.winner_of = mock.MagicMock(return_value="x")
@@ -16,18 +30,16 @@ class ResponseHandlerTests(unittest.TestCase):
 
 class IntegrationWithMoveGeneratorTests(unittest.TestCase):
 
-   def test_response_delegates_to_move_generator(self):
-       data = {"board": {1:"x",2:"x",5:"o"},
+   def test_response_contains_updated_game_information(self):
+       data = {"board": {1:"o", 2:"o",5:"x"},
                "depth": 20}
-       handler = ResponseHandler(MoveGenerator())
-       response = handler.response(data)
+       response = ResponseHandler(MoveGenerator()).response(data)
        self.assertEqual(3,response["move"])
-       self.assertEqual(None,response["winner"])
+       self.assertEqual("o",response["winner"])
+       self.assertEqual(None,response["winner_on_board"])
 
    def test_handler_defaults_difficulty_if_depth_not_set(self):
        data = {"board": {1:"x",2:"x",5:"o"}}
        handler = ResponseHandler(MoveGenerator())
        response = handler.response(data)
        self.assertEqual(3,response["move"])
-
-    
