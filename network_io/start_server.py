a1611f00ca7fff8580b245b2b8bed4d1cb911563
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-10-27 00:27:12 -0500

    Adds multithreading support in main loop

--- network_io/start_server.py
+++ network_io/start_server.py
@@ -3,13 +3,19 @@ from json_transmitter import JsonTransmitter
 from responder import Responder
 from move_generator import MoveGenerator
 from response_handler import ResponseHandler
+from response_thread import ResponseThread
 
 server = ServerSocket("localhost",5000)
 server.initialize_and_listen_for_connections()
 
 while True:
-    connection_socket = server.accept_connection_and_return_socket()
-    transmitter = JsonTransmitter(connection_socket)
-    responder = Responder(transmitter,ResponseHandler(MoveGenerator()))
-    responder.respond()
-    connection_socket.close()
+    try:
+      connection_socket = server.accept_connection_and_return_socket()
+      transmitter = JsonTransmitter(connection_socket)
+      move_generator = MoveGenerator()
+      handler = ResponseHandler(move_generator)
+      responder = Responder(transmitter, handler)
+      ResponseThread(responder).start()
+    except KeyboardInterrupt:
+      print "Shutting down server..."
+
